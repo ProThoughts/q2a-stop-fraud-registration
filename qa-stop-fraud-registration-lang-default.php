383b3d84fd9c0b59bd134b677342bbec5120de84
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-30 15:21:44 +0900

    Add translation files

---
+++ qa-stop-fraud-registration-lang-default.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+	Plugin Name: Stop Fraud Registration
+	Plugin URI:
+	Plugin Description: To stop the new user registration When register more than a certain number within a set time
+	Plugin Version: 1.0
+	Plugin Date: 2016-06-30
+	Plugin Author:38qa.net
+	Plugin Author URI:
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+	return array(
+		// default
+		'within_hours' => 'Within hours:',
+		'max_registers' => 'Max Registars:'
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
---
+++ qa-stop-fraud-registration-lang-ja.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+	Plugin Name: Stop Fraud Registration
+	Plugin URI:
+	Plugin Description: To stop the new user registration When register more than a certain number within a set time
+	Plugin Version: 1.0
+	Plugin Date: 2016-06-30
+	Plugin Author:38qa.net
+	Plugin Author URI:
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+	return array(
+		// japanese
+		'within_hours' => '何時間以内に:',
+		'max_registers' => '上記時間内にこの人数を超えた場合新規登録を停止する<br>人数:'
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
